5cc0c05c0bb2872ad4f39255e22c2ed1cf242e31
genki81 <thomas.billard@gmail.com>
2015-11-03 20:56:52 +0100

    factorisation

--- src/main/webapp/js/tabard.js
+++ src/main/webapp/js/tabard.js
@@ -1,3 +1,59 @@
+/**
+ * Core global functionality.
+ */
+var Core = {
+
+	/**
+	 * Zero-fills a number to the specified length (works on floats and negatives, too).
+	 *
+	 * @param number
+	 * @param width
+	 * @param includeDecimal
+	 * @return string
+	 */
+	zeroFill: function(number, width, includeDecimal) {
+		if (typeof includeDecimal === 'undefined') {
+			includeDecimal = false;
+		}
+
+		var result = parseFloat(number),
+			negative = false,
+			length = width - result.toString().length,
+			i = length - 1;
+
+		if (result < 0) {
+			result = Math.abs(result);
+			negative = true;
+			length++;
+			i = length - 1;
+		}
+
+		if (width > 0) {
+			if (result.toString().indexOf('.') > 0) {
+				if (!includeDecimal) {
+					length += result.toString().split('.')[1].length;
+				}
+
+				length++;
+				i = length - 1;
+			}
+
+			if (i >= 0) {
+				do {
+					result = '0' + result;
+				} while (i--);
+			}
+		}
+
+		if (negative) {
+			return '-' + result;
+		}
+
+		return result;
+	}
+
+};
+
 function GuildTabard(id, tabard) {
 	var self = this,
 		canvas = document.getElementById(id),
